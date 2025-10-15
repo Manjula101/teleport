f0aad6c4e1eae91cebb3bf4727ed5b8b28436ff9
hugoShaka <hugo.hervieux@goteleport.com>
2025-10-15 11:23:18 -0400

    Make IsAccessListMember cycle-proof

--- lib/accesslists/hierarchy_test.go
+++ lib/accesslists/hierarchy_test.go
@@ -430,7 +430,6 @@ func TestAccessListIsMember_NestedRequirements(t *testing.T) {
 	})
 
 	t.Run("cyclic graph, no membership", func(t *testing.T) {
-		t.Skip("cyclic graph not supported yet")
 		firstList := newAccessList(t, "first", clock)
 		secondList := newAccessList(t, "second", clock)
 		thirdList := newAccessList(t, "third", clock)
@@ -473,7 +472,6 @@ func TestAccessListIsMember_NestedRequirements(t *testing.T) {
 	})
 
 	t.Run("cyclic graph, user membership", func(t *testing.T) {
-		t.Skip("cyclic graph not supported yet")
 		firstList := newAccessList(t, "first", clock)
 		secondList := newAccessList(t, "second", clock)
 		thirdList := newAccessList(t, "third", clock)
